70a87001c06e68c0a6c686c6f6d862e4ca77333d
Reinaldo Taslim <reinaldotaslim@Reinaldos-iMac.local>
2020-05-12 10:39:33 -0700

    Initial Commit

---
+++ IndividualAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndividualAssignment
+//
+//  Created by Reinaldo Taslim on 12/05/2020.
+//  Copyright © 2020 Reinaldo Taslim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
